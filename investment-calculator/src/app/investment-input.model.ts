0470bdfa831af1417d9eba567d41b37693e73904
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-17 00:54:40 +0330

    investment-calculator/complete project components

---
+++ investment-calculator/src/app/investment-input.model.ts
@@ -0,0 +1,15 @@
+export type InvestmentInput = {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+}
+
+export type InvestmentOutput = {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+}
