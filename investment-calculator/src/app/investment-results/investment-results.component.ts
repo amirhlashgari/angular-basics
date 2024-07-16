87f22cdf3c4b7f11c343e916966dcc0eb3728b5f
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-17 01:17:36 +0330

    investment-calculator/using signals and services instead of events

--- investment-calculator/src/app/investment-results/investment-results.component.ts
+++ investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, input, Input } from '@angular/core';
 
 import { InvestmentOutput } from '../investment-input.model';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input() results?: InvestmentOutput[];
+  // @Input() results?: InvestmentOutput[];
+  private investmentService = inject(InvestmentService);
+  
+  get results() {
+    return this.investmentService.resultsData;
+  }
 }
--- investment-calculator/src/app/user-input/user-input.component.ts
+++ investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, output, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,14 +11,18 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  constructor(private investmentService: InvestmentService) {}
 
   onSubmit() {
-    this.calculate.emit({ initialInvestment: +this.enteredInitialInvestment, annualInvestment: +this.enteredAnnualInvestment, expectedReturn: +this.enteredExpectedReturn, duration: +this.enteredDuration })
+    this.investmentService.calculateInvestmentResults({ initialInvestment: +this.enteredInitialInvestment(), annualInvestment: +this.enteredAnnualInvestment(), expectedReturn: +this.enteredExpectedReturn(), duration: +this.enteredDuration() });
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
