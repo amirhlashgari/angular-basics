884b5e45f0c613503cab10dcdcfcc869db9353a4
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-19 08:50:59 +0330

    change-detection/ using OnPush change detection to minimize number of rerendered components if it wasn't relevant

--- change-detection/src/app/messages/messages.component.ts
+++ change-detection/src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 import { MessagesListComponent } from './messages-list/messages-list.component';
 import { NewMessageComponent } from './new-message/new-message.component';
@@ -9,6 +9,8 @@ import { NewMessageComponent } from './new-message/new-message.component';
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css',
   imports: [MessagesListComponent, NewMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,  // would cause limitation on amount of events and thisngs that could trigger change detectionyou shouldn't use onPush if your changes would affect all components
+  // NOTE: but if this component changes it would cause others to be checked, if you don't want so you should implement onPush on all irrelevant components
 })
 export class MessagesComponent {
   messages = signal<string[]>([]);
