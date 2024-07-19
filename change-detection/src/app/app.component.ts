d6e1d89e86f7139cb353070deecc028dce199e71
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-19 08:49:31 +0330

    change-detection/ notes about change detection mechanism during developement and best practices

--- change-detection/src/app/app.component.ts
+++ change-detection/src/app/app.component.ts
@@ -14,5 +14,14 @@ export class AppComponent {
   get debugOutput() {
     console.log('[AppComponent] "debugOutput" binding re-evaluated.');
     return 'AppComponent Component Debug Output';
+    /**
+     * return Math.random()
+     * if you see logs you will see that all the logs acccuired will execute twice, 
+     * this will happen only in developement and if you build --prod this behavior won't happen. this is for 
+     * rechecking the attributes and if you return a random number as first line it would cause an error in the 
+     * second round of checkings.
+     * IMPORTANT: therefore you shouldn't put expensive calculations into templates, 
+     * the expressions in your template-binding should be simple and staight forward.
+     */
   }
 }
