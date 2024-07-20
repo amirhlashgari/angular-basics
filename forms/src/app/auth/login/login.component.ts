50b47e0397464e23c81d75edee0037cc540d0ce7
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-21 03:29:31 +0330

    forms/ interacting with form programmatically

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
     if (control.value.includes('?')) {
@@ -18,6 +18,13 @@ function emailIsUnique(control: AbstractControl) {
     return of({ notUnique: true });
 }
 
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem('saved-login-form');
+if (savedForm) {
+    const loadedForm = JSON.parse(savedForm);
+    initialEmailValue = loadedForm.email;
+}
+
 @Component({
     selector: 'app-login',
     standalone: true,
@@ -25,9 +32,10 @@ function emailIsUnique(control: AbstractControl) {
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+    private destroyRef = inject(DestroyRef);
     form = new FormGroup({
-        email: new FormControl('', {
+        email: new FormControl(initialEmailValue, {
             validators: [Validators.email, Validators.required],
             asyncValidators: [emailIsUnique]
         }),
@@ -42,6 +50,18 @@ export class LoginComponent {
 
     get passwordIsInvalid() {
         return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid);
+    }
+
+    ngOnInit(): void {
+
+
+        const subscription = this.form.valueChanges.pipe(debounceTime(500)).subscribe({
+            next: (value) => {
+                window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email })); // to save form in unwanted refreshs 
+            }
+        });
+
+        this.destroyRef.onDestroy(() => subscription.unsubscribe());
     }
 
     onSubmit() {
