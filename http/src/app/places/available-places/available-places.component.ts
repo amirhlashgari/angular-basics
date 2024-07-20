e6fc43ae2b68ec0de04a2b3732dd505285b1b53c
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-20 13:31:31 +0330

    http/ adding fallback state and loading to component

--- http/src/app/places/available-places/available-places.component.ts
+++ http/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -14,12 +15,22 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
-      next: (resData) => { }
+    this.isFetching.set(true);
+    // this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places', { observe: 'events | response' }) ---> this option would cause see full response object
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+    .pipe(map((resData) => resData.places)) // map is rxjs operator, that changes data before reaching to subscribe. all of them can be applied using pipe()
+    .subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(true);
+      }
     });
 
     this.destroyRef.onDestroy(() => {
