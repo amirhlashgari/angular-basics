7c45ae6cccdc2e1da9f3284bfc1a3b48273ef620
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-21 03:18:16 +0330

    form/ adding validation to form, using custom and built-in validators and async validation(mostly used for backend check)

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+    if (control.value.includes('?')) {
+        return null;
+    }
+
+    return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+    if (control.value !== 'test@test.com') {
+        return of(null); // NOTE: of() operator emits an Observable(here is to simulate async validation from backend)
+    }
+
+    return of({ notUnique: true });
+}
 
 @Component({
     selector: 'app-login',
@@ -10,9 +27,26 @@ import { ReactiveFormsModule, FormControl, FormGroup, NgForm } from '@angular/fo
 })
 export class LoginComponent {
     form = new FormGroup({
-        email: new FormControl(''),
-        password: new FormControl('')
+        email: new FormControl('', {
+            validators: [Validators.email, Validators.required],
+            asyncValidators: [emailIsUnique]
+        }),
+        password: new FormControl('', {
+            validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
+        })
     });
 
-    onSubmit(formData: NgForm) { }
+    get emailIsInvalid() {
+        return (this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid);
+    }
+
+    get passwordIsInvalid() {
+        return (this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid);
+    }
+
+    onSubmit() {
+        // console.log(this.form) ---> this object is as TemplateDriven approach, but typescript now understands our shape of form now
+        const enteredEmail = this.form.value.email;
+        const enteredPassword = this.form.value.password;
+    }
 }
