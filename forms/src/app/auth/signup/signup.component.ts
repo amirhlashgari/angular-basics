34d76a89e08194f1e63f7d4d87cac759c700367b
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-21 04:17:15 +0330

    forms/ add validator factory to FormGroup and validate values equality

--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function equalValues(controlName1: string, controlName2: string) {
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+    if (val1 === val2) {
+      return null;
+    }
+    return { valuesNotEqual: true };
+  }
+
+
+}
 
 @Component({
   selector: 'app-signup',
@@ -14,6 +27,8 @@ export class SignupComponent {
     passwords: new FormGroup({
       password: new FormControl('', { validators: [Validators.required, Validators.minLength(6)] }),
       confirmPassword: new FormControl('', { validators: [Validators.required, Validators.minLength(6)] }),
+    }, { 
+      validators: [equalValues('password', 'confirmPassword')] // would validate the formGroup and checks two fields equality
     }),
     firstName: new FormControl('', { validators: [Validators.required] }),
     lastName: new FormControl('', { validators: [Validators.required] }),
