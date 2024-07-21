37186229243df9461a9a35321a126fbfc732a029
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-21 09:44:29 +0330

    routing/ child routes

--- routing/src/app/app.config.ts
+++ routing/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes, withComponentInputBinding()) // NOTE: withComponentInputBinding helps to fetch our dynamic data through input()s if the name of it be the same
+        provideRouter(
+            routes, 
+            withComponentInputBinding(), // NOTE: withComponentInputBinding helps to fetch our dynamic data through input()s if the name of it be the same
+            withRouterConfig({ paramsInheritanceStrategy: 'always' }) // used to get access to current full url in the child components
+        )
     ],
 };
