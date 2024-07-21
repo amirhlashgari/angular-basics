2c674d74ab4fb72b0fb6817906f2de0a339bd9f4
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-21 09:22:26 +0330

    routing/ extract dynamic route parameters

--- routing/src/app/app.config.ts
+++ routing/src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes)],
+    providers: [
+        provideRouter(routes, withComponentInputBinding()) // NOTE: withComponentInputBinding helps to fetch our dynamic data through input()s if the name of it be the same
+    ],
 };
