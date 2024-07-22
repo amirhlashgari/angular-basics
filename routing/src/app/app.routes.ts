27f364b6411b13d4c5dfb5f697c02d2c94478307
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-22 04:57:24 +0330

    routing/ adding redirects and fallback cases

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
     {
@@ -15,6 +16,11 @@ export const routes: Routes = [
         component: UserTasksComponent,
         children: [
             {
+                path: '',
+                redirectTo: 'tasks',
+                pathMatch: 'prefix'
+            },
+            {
                 path: 'tasks',
                 component: TasksComponent
             },
@@ -24,4 +30,8 @@ export const routes: Routes = [
             }
         ]
     },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
 ];
