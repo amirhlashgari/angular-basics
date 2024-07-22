6ace3e7a4bc36196810c84985bbd568ec83c5260
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2024-07-22 05:25:41 +0330

    routing/ use query params to sort user tasks

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
@@ -9,13 +9,27 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   // userId = input.required<string>(); ---> IMPORTANT: normally child routes doesn't get the part of route that is not related inside their 'path' but it could be solved using "paramsInheritanceStrategy: always"
   userId = input.required<string>();
+  // order = input<'asc'|'desc'>(); ---> instead of all logic used inside ngOnInit, we could use this line. 
+  order = signal<'asc' | 'desc'>('desc');
   private tasksService = inject(TasksService);
-  userTasks = computed(() => this.tasksService.allTasks().filter(
-    task => task.userId === this.userId()
-  ));
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  userTasks = computed(() => this.tasksService.allTasks()
+    .filter(task => task.userId === this.userId())
+    .sort((a, b) => this.order() === 'desc' ? (a.id > b.id ? -1 : 1) : (a.id > b.id ? 1 : -1))
+  );
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: param => this.order.set(param['order'])
+    })
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
